f632e05a3c23d6576cea25cb5ea3395adf766302
Xurshid <sotiboldiyevxurshid21@gmail.com>
2023-01-06 18:11:28 +0500

    ff

--- src/componet/Button.js
+++ src/componet/Button.js
@@ -5,13 +5,13 @@ import './stule.css'
 export default function Button() {
 
     const [count,setCount] = useState(1)
-
+     
   return (
     <div style={{display:"flex",justifyContent:"center"}}>
     <div className='box'>
          <button onClick={() => setCount(count + 1)}>+</button>
          </div>
-     <h1>men yoshim {count}</h1>
+     <h1>men yoshim {count} da</h1>
      <div className='box'>
          <button  onClick={() => setCount(count - 1)}>-</button>
          </div>
